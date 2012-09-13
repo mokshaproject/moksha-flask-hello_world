0a0ee01e8d2f219254349f2a947e682e42ee73b3
Ralph Bean <rbean@redhat.com>
2012-09-13 14:39:02 -0400

    Your first live widget.

--- tutorial.py
+++ tutorial.py
@@ -4,15 +4,46 @@ from tw2.core.middleware import make_middleware
 
 import datetime
 import moksha.hub.api.producer
-
+import moksha.wsgi.widgets.api
+import tw2.jqplugins.gritter
+import flask.templating
 
 from flask import Flask
 app = Flask(__name__)
 
+simple_template = """
+<html>
+<head></head>
+<body>
+Really?
+{{notification_widget.display()}}
+{{moksha_socket.display()}}
+</body>
+</html>
+"""
+
+
+class PopupNotification(moksha.wsgi.widgets.api.LiveWidget):
+    topic = "*"
+    onmessage = "$.gritter.add({'title': 'Received', 'text': json});"
+    resources = moksha.wsgi.widgets.api.LiveWidget.resources + \
+            tw2.jqplugins.gritter.gritter_resources
+    backend = "websocket"
+
+    # Don't actually produce anything when you call .display() on this widget.
+    inline_engine_name = "mako"
+    template = ""
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    config = get_moksha_appconfig()
+    socket = moksha.wsgi.widgets.api.get_moksha_socket(config)
+    return flask.templating.render_template_string(
+        simple_template,
+        notification_widget=PopupNotification,
+        moksha_socket=socket,
+    )
 
 
 class HelloWorldProducer(moksha.hub.api.producer.PollingProducer):
@@ -20,6 +51,7 @@ class HelloWorldProducer(moksha.hub.api.producer.PollingProducer):
 
     def poll(self):
         self.send_message('hello_world', "Hello World!")
+
 
 if __name__ == "__main__":
     # Load development.ini
@@ -29,4 +61,4 @@ if __name__ == "__main__":
     app.wsgi_app = make_moksha_middleware(app.wsgi_app, config)
     app.wsgi_app = make_middleware(app.wsgi_app)
 
-    app.run()
+    app.run(debug=True)
