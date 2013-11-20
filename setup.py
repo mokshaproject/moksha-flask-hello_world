14b8d5bc64d9eaaf86a48a6dc9b279f8243a46ce
RemyD <remyd@civx.us>
2013-11-19 20:49:22 -0500

    Added install_requires to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
 setup(
     name='tutorial',
+    install_requires=[
+            "flask",
+            "moksha.wsgi",
+            "moksha.hub",
+            ],
     entry_points="""
     [moksha.producer]
     hello = tutorial:HelloWorldProducer
